a6cbf861b5c4368d707e0de677df253f566b6f13
MUSTAFAYAS�N BAYIR <2013010206070@ogrenci.karabuk.edu.tr>
2022-08-17 21:21:24 +0300

    Entities katmanında Abstract,Concrete,Dtos ve Enum klasörleri oluşturuldu.

---
+++ Entities/Abstract/IUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    public interface IUpdate
+    {
+        public int? ModificationBy { get; set; }
+        public DateTime? ModificationDate { get; set; }
+    }
+}
---
+++ Entities/Concrete/BaseEntities/AuditTableEntity.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditTableEntity : BaseEntity, ICreated,IUpdate
+    {
+        public int CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int? ModificationBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? ModificationDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
